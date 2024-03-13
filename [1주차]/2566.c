588d153d7b7e9c30642f29d6bcc9118e7eaf41e2
YoungseoChoi <dudtj9326@ewhain.net>
2024-03-13 11:03:09 +0900

    [BOJ] 2566 / 출처  https://www.acmicpc.net/problem/2566

---
+++ [1주차]/2566.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int num,x,y;
+int max=-1;
+
+int main(){
+    for(int i=1; i<=9; i++){
+        for(int k=1; k<=9 ; k++){
+            scanf("%d",&num);
+            
+            if(max<=num){
+                max=num;
+                x=i;
+                y=k;
+            }
+        }
+    }
+    printf("%d \n%d %d", max, x,y);
+    return 0;
+}
