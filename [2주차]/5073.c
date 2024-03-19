39633a00cbbd43ea1e804296cea9a5d51aa31022
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-03-19 13:04:10 +0900

    [BOJ] 삼각형과 세 변 / 출처 https://www.acmicpc.net/problem/5073

--- [2주차]/5073.c
+++ [2주차]/5073.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int max(int a, int b, int c){
-    if(a>b){
-        if(a>c) return a;
+    if(a > b){
+        if(a > c) return a;
         else return c;
     }
     else{
-        if(b>c) return b;
+        if(b > c) return b;
         else return c;
     }
 }
