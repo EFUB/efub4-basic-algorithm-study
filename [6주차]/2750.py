364b22cf10427c17a3c6812d33241c3b35caffdf
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-05-18 23:03:42 +0900

    [BOJ] 로봇청소기 / 출처 : https://www.acmicpc.net/problem/14503

--- [6주차]/2750.py
+++ [6주차]/2750.py
@@ -5,12 +5,13 @@ for i in range(n):
     m.append(int(input()))
     
 # insert sort
-for i in range(2, len(m)):
+for i in range(1, len(m)):
     key=m[i]
     j=i-1
-    while(j>0) & (m[j]>key):
+    while(j>=0) and (m[j]>key):
         m[j+1]=m[j]
         j-=1
+    m[j+1]=key
         
 for n in m:
     print (n)
