25ce4fc428a29c2d858c852711cc5b724e6de386
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-06-30 23:15:08 +0900

    [BOJ] 주식 / https://www.acmicpc.net/problem/11501

---
+++ [9주차]/11501.py
@@ -0,0 +1,14 @@
+testCase=int(input())
+for i in range(testCase):
+    n=int(input())
+    price=list(map(int,input().split()))
+    
+    money=0
+    maxPrice=0
+    
+    for i in range(len(price)-1, -1,-1):
+        if price[i]>maxPrice:
+            maxPrice=price[i]
+        else:
+            money += maxPrice-price[i]
+    print(money)
